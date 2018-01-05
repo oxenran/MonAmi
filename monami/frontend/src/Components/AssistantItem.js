5b5af7baa15513e3c403a02b53299aef1a8aeabf
Kimberley Zell <kimberley.zell@gmail.com>
2018-01-05 11:21:20 -0800

    messing with styling

--- monami/frontend/src/Components/AssistantItem.js
+++ monami/frontend/src/Components/AssistantItem.js
@@ -3,12 +3,23 @@ import React, { Component } from 'react';
 class AssistantItem extends React.Component {
   render(){
     return(
-      <li className="AssistantItem">
-
-        <p>{this.props.assistant.first_name} {this.props.assistant.last_name}</p>
-      </li>
+      <div className="AssistantItemStyle">
+        <ul>
+          <li>
+            {this.props.assistant.first_name} {this.props.assistant.last_name}
+          </li>
+        </ul>
+      </div>
     );
   }
 }
+
+
+const AssistantItemStyle = {
+  width: 200,
+  height: 250,
+  display: 'inline-block',
+}
+
 
 export default AssistantItem;
