11853d068ef7535521b3b184b3c4ba4154e2dead
Kimberley Zell <kimberley.zell@gmail.com>
2018-01-09 11:22:14 -0800

    installed bootstrap

--- monami/frontend/src/index.js
+++ monami/frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
