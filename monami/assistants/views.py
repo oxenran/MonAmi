8f4193c33a2bba823d358ce86fef796bec7c7dae
Roxanne <argonkryptonneon@gmail.com>
2018-01-09 12:58:11 -0800

    WOOOO Filtering

--- monami/assistants/views.py
+++ monami/assistants/views.py
@@ -5,18 +5,23 @@ from assistants.serializers import AssistantSerializer
 from assistants.serializers import UserSerializer
 from assistants.serializers import AppointmentSerializer
 from django.http import Http404
+import django_filters.rest_framework
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 class AssistantList(generics.ListCreateAPIView):
     queryset = Assistant.objects.all()
     serializer_class = AssistantSerializer
+    # filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('created', 'last_name', 'first_name', 'household', 'companion', 'driver')
 
 
 class AssistantDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -26,7 +31,9 @@ class AssistantDetail(generics.RetrieveUpdateDestroyAPIView):
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
+    # filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('username')
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
@@ -35,19 +42,32 @@ class UserDetail(generics.RetrieveAPIView):
 class AppointmentList(generics.ListCreateAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
+    # filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('date', 'owner')
 
 
 class AppointmentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
 
-
+#
 # @api_view(['GET'])
 # def api_root(request, format=None):
 #     return Response({
 #         'users': reverse('user-list', request=request, format=format),
-#         'assistants': reverse('assistant-list', request=request, format=format)
+#         'assistants': reverse('assistant-list', request=request, format=format),
+#         'appointments': reverse('appointment-list', request=request, format=format),
 #     })
+#
+# class AssistantHighlight(generics.GenericAPIView):
+#     queryset = Assistant.objects.all()
+#     renderer_classes = (renderers.StaticHTMLRenderer,)
+#
+#     def get(self, request, *args, **kwargs):
+#         assistant = self.get_object()
+#         return Response(assistant.highlighted)
+
 # class AssistantList(mixins.ListModelMixin,
 #                   mixins.CreateModelMixin,
 #                   generics.GenericAPIView):
