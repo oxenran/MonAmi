9aa2fbd1c312d91bcfe4f4e5f4f6a146b169a9b4
Roxanne <argonkryptonneon@gmail.com>
2018-01-09 11:46:42 -0800

    fixed field in user serializer

--- monami/assistants/serializers.py
+++ monami/assistants/serializers.py
@@ -20,7 +20,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'email')
+        fields = ('id', 'first_name', 'last_name', 'email', 'username')
 
 class AppointmentSerializer(serializers.ModelSerializer):
     appointments = serializers.PrimaryKeyRelatedField(many=True, queryset=Appointment.objects.all())
