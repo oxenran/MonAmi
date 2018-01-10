f30bc40493f3c5b31a00789d85c47305592ed9c6
Roxanne <argonkryptonneon@gmail.com>
2018-01-10 12:42:04 -0800

    added field to serializer

--- monami/assistants/serializers.py
+++ monami/assistants/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from assistants.models import Assistant
 from assistants.models import Appointment
 from django.contrib.auth.models import User
-# LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class AssistantSerializer(serializers.ModelSerializer):
@@ -10,21 +9,16 @@ class AssistantSerializer(serializers.ModelSerializer):
     class Meta:
         appointments = serializers.PrimaryKeyRelatedField(many=True, queryset=Appointment.objects.all())
         model = Assistant
-        fields = ('id', 'first_name', 'last_name', 'email', 'household', 'driver', 'companion', 'image_url')
-        #add 'appointments' into field later
-
-
+        fields = ('id', 'first_name', 'last_name', 'email', 'household', 'driver', 'companion', 'image_url', 'appointments')
 
 class UserSerializer(serializers.ModelSerializer):
 
     #this is how you add a primary key
-    # assistants = serializers.PrimaryKeyRelatedField(many=True, queryset=Assistant.objects.all())
-    #after this you'd add 'assistants' to the field below
+    appointments = serializers.PrimaryKeyRelatedField(many=True, queryset=Appointment.objects.all())
 
     class Meta:
         appointments = serializers.PrimaryKeyRelatedField(many=True, queryset=Appointment.objects.all())
         model = User
-        #possibly add appointments to fields
         fields = ( 'id', 'username', 'email', 'appointments')
 
 class AppointmentSerializer(serializers.ModelSerializer):
