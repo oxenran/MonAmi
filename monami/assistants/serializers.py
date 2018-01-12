f74e8222ddd8d7f28375accf3bc397cbe8dd0ded
Roxanne <argonkryptonneon@gmail.com>
2018-01-12 13:02:00 -0800

    validator to ensure that enough time is given between appointments

--- monami/assistants/serializers.py
+++ monami/assistants/serializers.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from rest_framework.validators import UniqueTogetherValidator
 from django.core.exceptions import ValidationError
-from datetime import datetime
+from rest_framework.validators import UniqueForDateValidator
+from datetime import datetime, timezone
 from datetime import timedelta
 
 class AssistantSerializer(serializers.ModelSerializer):
@@ -34,29 +35,33 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+def time_warp(datetime):
+    #checks to see that you have scheduled your appointment at a time greater than two days from now
+    if datetime < (datetime.now(timezone.utc) + timedelta(days = 2)):
+        raise ValidationError('You need to book this appointment at least two days ahead.')
+    return datetime
+
 class AppointmentSerializer(serializers.ModelSerializer):
 
     owner = serializers.ReadOnlyField(source='owner.username')
+    date = serializers.DateTimeField(validators=[time_warp])
     # assistant = serializers.ReadOnlyField(source='assistant.id')
     # queryset = Appointment.objects.filter(owner=request.user)
     class Meta:
         model = Appointment
-
         fields = ('id', 'date', 'details', 'assistant', 'owner')
-        def appointment_conflict(self):
-            if 'date' > datetime.now + timedelta(days = 2):
-                raise ValidationError('You need to book this appointment at least two days ahead.')
-        # def validate_date(self, date):
-        #     print(date)
-        #     print('that is the date')
-        #     if date > (datetime.now + timedelta(days = 2)):
-        #         raise serializers.ValidationError('You need to book this appointment at least two days ahead.')
-        #     return date
         validators = [
             UniqueTogetherValidator(
                 queryset=Appointment.objects.all(),
                 fields=('assistant', 'date')
             ),
+            # UniqueForDateValidator(
+            #     queryset=Appointment.objects.all(),
+            #     field = 'owner',
+            #     date_field='date',
+            #     message = 'The User already as an appointment at this time'
+            # ),
+
             #validate that date > datetime.now + timedelta(days= 2)
             # UniqueTogetherValidator(
             #     queryset=Appointment.objects.all(),
