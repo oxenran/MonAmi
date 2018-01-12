40d978cf1bfb71da2b644a3012a7d56d07cdebe5
Kimberley Zell <kimberley.zell@gmail.com>
2018-01-12 14:42:52 -0800

    working on it

--- monami/frontend/src/Components/AppointmentBookingForm.js
+++ monami/frontend/src/Components/AppointmentBookingForm.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ServicesCheckbox from './ServicesCheckbox';
 import App from '../App';
+import 'react-dates/initialize';
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 // import ReactDatetime from 'react-datetime'
 
 // const services = {
