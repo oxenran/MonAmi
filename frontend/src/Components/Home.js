f421a1bafe5fd57b328a304a9aa88dd426c72537
Kimberley Zell <kimberley.zell@gmail.com>
2018-01-21 19:49:50 -0800

    homepage clickbox

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -5,10 +5,10 @@ import { Image, Col } from 'react-bootstrap';
 const Home = () => (
   <div className="homepage">
     <Image id="homepage-background" src="https://images.unsplash.com/photo-1476611317561-60117649dd94?auto=format&fit=crop&w=1050&q=80" responsive />
-    <Col xs={12} md={10} lg={8}>
-    <div className="clickbox">
+    <Col className="clickbox" xs={12} md={10} lg={8}>
+
       <h2 className="clickbox-text"><Link to='/Assistants'>In a world full of people, no one should be alone. Find a friend who can help now!</Link></h2>
-    </div>
+  
     </Col>
   </div>
 )
