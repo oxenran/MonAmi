cf2e16fcacc6b9dce03c2caa45bfe3149f60350f
Roxanne <argonkryptonneon@gmail.com>
2018-01-22 10:33:53 -0800

    setting up user profile

--- monami/assistants/models.py
+++ monami/assistants/models.py
@@ -16,22 +16,32 @@ from rest_framework.authtoken.models import Token
 #         )
 #
 
-class Profile(models.Model):
+class Assistant(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    user = models.OneToOneField('auth.User', related_name='profile', on_delete=models.CASCADE)
+    user = models.OneToOneField('auth.User', related_name='assistant', on_delete=models.CASCADE)
     last_name = models.CharField(blank=False, max_length=100)
     first_name = models.CharField(blank=False, max_length=100)
-    bio = models.TextField(blank=True, null=True, default=0)
+    household = models.BooleanField(default=False)
+    driver = models.BooleanField(default=False)
+    companion = models.BooleanField(default=False)
     image_url = models.CharField(max_length=500, default= "https://i.imgur.com/yILM61G.jpg")
     # owner = models.ForeignKey('auth.User', related_name='assistants', on_delete=models.CASCADE)
     # highlighted = models.TextField()
 
     class Meta:
         ordering = ('created',)
-class Assistant(Profile):
-    household = models.BooleanField(default=False)
-    driver = models.BooleanField(default=False)
-    companion = models.BooleanField(default=False)
+
+
+class Profile(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.OneToOneField( 'auth.User',                     related_name='profile', on_delete=models.CASCADE)
+    last_name = models.CharField(blank=False, max_length=100)
+    first_name = models.CharField(blank=False, max_length=100)
+    image_url = models.CharField(max_length=500, blank=True, null=True)
+    address = models.TextField(blank=True, null=True, max_length=200)
+
+    class Meta:
+        ordering = ('created',)
 
 class Appointment(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -39,22 +49,11 @@ class Appointment(models.Model):
     owner = models.ForeignKey( 'auth.User',                     related_name='appointments', on_delete=models.CASCADE)
     assistant = models.ForeignKey(Assistant, related_name='appointments', on_delete=models.CASCADE)
     # assistant = models.ForeignKey('models.Assistant',                        related_name='assistants', on_delete=models.CASCADE)
-    details = models.TextField(blank=True, null=True, default='')
+    details = models.TextField(blank=True, null=True, default=0)
 
     class Meta:
         ordering = ('date',)
 
-# @receiver(post_save, sender=User)
-# def create_user_assistant(sender, instance, created, **kwargs):
-#     if created:
-#         Assistant.objects.create(user=instance)
-
-# @receiver(post_save, sender=User)
-# def save_user_assistant(sender, instance, **kwargs):
-#     instance.assistant.save()
-
-# for user in User.objects.all():
-#     Token.objects.get_or_create(user=user)
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
