28e6e8acd43df14697724d1e9b8bb6640407d2e2
Kimberley Zell <kimberley.zell@gmail.com>
2018-01-21 21:02:53 -0800

    cropped homepage image again

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -4,7 +4,7 @@ import { Image, Col } from 'react-bootstrap';
 
 const Home = () => (
   <div className="homepage">
-    <Image id="homepage-background" src="https://image.ibb.co/bu4BKG/homepageimage.jpg"></Image>
+    <Image id="homepage-background" src="https://image.ibb.co/mC9xRw/homepageimage.jpg"></Image>
     <Col className="clickbox" xs={12} md={10} lg={8}>
 
       <h2 className="clickbox-text"><Link to='/Assistants'>In a world full of people, no one should be alone. Find a friend who can help now!</Link></h2>
